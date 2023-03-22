2359cc54ca69b1119d4f67b319f541efccc438df
jung <JjeunEeunAae@naver.com>
2023-03-22 17:24:52 +0900

    20230322 spring test

---
+++ ex230322/src/main/java/com/yedam/app/emp/service/EmpVO.java
@@ -0,0 +1,15 @@
+package com.yedam.app.emp.service;
+
+import lombok.Data;
+
+@Data
+public class EmpVO {
+	String employee_id;
+	String first_name;
+	String last_name;
+	String eamil;
+	String hire_date;
+	String jod_id;
+	String department_id;
+	String salary;
+}
