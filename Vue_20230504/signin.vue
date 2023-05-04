f3640582e2ff3c0b52688d54ac4b8cb287933c64
jung <JjeunEeunAae@naver.com>
2023-05-04 10:09:51 +0900

    20230503~0504_eunae

---
+++ Vue_20230504/signin.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <h2>회원가입</h2>
+    <input><br>
+    <input><br>
+    <input><br>
+    <input><br>
+    <button @click="register" type="button">등록</button>
+</div>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    },
+    methods : {
+        register(){
+            console.log("가입완료");
+            this.$router.push("/login");
+        }
+    }
+}
+</script>
+
+<style>
+.hello {
+    background-color: #ffe;
+}
+</style>
